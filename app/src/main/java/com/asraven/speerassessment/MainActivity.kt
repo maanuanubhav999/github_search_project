62990808f5f0a5e596b707a306988798c5d0b5ec
anubhav <anubhav@sportskeeda.com>
2024-01-15 21:09:10 +0530

    replace main screen

--- app/src/main/java/com/asraven/speerassessment/MainActivity.kt
+++ app/src/main/java/com/asraven/speerassessment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.asraven.speerassessment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,8 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.asraven.speerassessment.ui.components.SearchScreen
 import com.asraven.speerassessment.ui.theme.SpeerAssessmentTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   SearchScreen()
                 }
             }
         }
